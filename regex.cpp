4cb66f0ba8c779005f4a0cc6c27dfa8b633692dc
李登淳 <lee@covariant.cn>
2021-03-23 13:51:38 +0800

    Update regex.cpp

--- regex.cpp
+++ regex.cpp
@@ -5,7 +5,7 @@
 * you may not use this file except in compliance with the License.
 * You may obtain a copy of the License at
 *
-* http://www.apache.org/licenses/LICENSE-2.0
+*     http://www.apache.org/licenses/LICENSE-2.0
 *
 * Unless required by applicable law or agreed to in writing, software
 * distributed under the License is distributed on an "AS IS" BASIS,
@@ -13,9 +13,11 @@
 * See the License for the specific language governing permissions and
 * limitations under the License.
 *
-* Copyright (C) 2019 Michael Lee(李登淳)
-* Email: mikecovlee@163.com
-* Github: https://github.com/mikecovlee
+* Copyright (C) 2017-2021 Michael Lee(李登淳)
+*
+* Email:   lee@covariant.cn, mikecovlee@163.com
+* Github:  https://github.com/mikecovlee
+* Website: http://covscript.org.cn
 */
 #include <regex>
 #include <covscript/dll.hpp>
