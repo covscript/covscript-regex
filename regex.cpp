6b7c113b324f2413f962df58b84e1bc314b9cf55
李登淳 <mikecovlee@163.com>
2023-01-07 20:41:18 +0800

    update regex

--- regex.cpp
+++ regex.cpp
@@ -1,56 +1,57 @@
 /*
-* Covariant Script Regex Extension
-*
-* Licensed under the Apache License, Version 2.0 (the "License");
-* you may not use this file except in compliance with the License.
-* You may obtain a copy of the License at
-*
-*     http://www.apache.org/licenses/LICENSE-2.0
-*
-* Unless required by applicable law or agreed to in writing, software
-* distributed under the License is distributed on an "AS IS" BASIS,
-* WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-* See the License for the specific language governing permissions and
-* limitations under the License.
-*
-* Copyright (C) 2017-2023 Michael Lee(李登淳)
-*
-* Email:   lee@covariant.cn, mikecovlee@163.com
-* Github:  https://github.com/mikecovlee
-* Website: http://covscript.org.cn
-*/
-#include <regex>
-#include <covscript/dll.hpp>
+ * Covariant Script Regex Extension
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
+ * Copyright (C) 2017-2023 Michael Lee(李登淳)
+ *
+ * Email:   lee@covariant.cn, mikecovlee@163.com
+ * Github:  https://github.com/mikecovlee
+ * Website: http://covscript.org.cn
+ */
 #include <covscript/cni.hpp>
+#include <covscript/dll.hpp>
+#include <regex>
 
-static cs::namespace_t regex_ext=cs::make_shared_namespace<cs::name_space>();
-static cs::namespace_t regex_result_ext=cs::make_shared_namespace<cs::name_space>();
+static cs::namespace_t regex_ext = cs::make_shared_namespace<cs::name_space>();
+static cs::namespace_t regex_result_ext =
+    cs::make_shared_namespace<cs::name_space>();
 
 namespace cs_impl {
-	template<>
+	template <>
 	cs::namespace_t &get_ext<std::regex>()
 	{
 		return regex_ext;
 	}
 
-	template<>
+	template <>
 	cs::namespace_t &get_ext<std::smatch>()
 	{
 		return regex_result_ext;
 	}
 
-	template<>
+	template <>
 	constexpr const char *get_name_of_type<std::regex>()
 	{
 		return "cs::regex";
 	}
 
-	template<>
+	template <>
 	constexpr const char *get_name_of_type<std::smatch>()
 	{
 		return "cs::regex::result";
 	}
-}
+}  // namespace cs_impl
 namespace regex_cs_ext {
 	using namespace cs;
 
@@ -88,24 +89,24 @@ namespace regex_cs_ext {
 		return m.empty();
 	}
 
-	number size(const std::smatch &m)
+	numeric size(const std::smatch &m)
 	{
 		return m.size();
 	}
 
-	number length(const std::smatch &m, number index)
+	numeric length(const std::smatch &m, numeric index)
 	{
-		return m.length(index);
+		return m.length(index.as_integer());
 	}
 
-	number position(const std::smatch &m, number index)
+	numeric position(const std::smatch &m, numeric index)
 	{
-		return m.position(index);
+		return m.position(index.as_integer());
 	}
 
-	string str(const std::smatch &m, number index)
+	string str(const std::smatch &m, numeric index)
 	{
-		return m.str(index);
+		return m.str(index.as_integer());
 	}
 
 	string prefix(const std::smatch &m)
@@ -118,7 +119,7 @@ namespace regex_cs_ext {
 		return m.suffix().str();
 	}
 
-	void init(name_space* ns)
+	void init(name_space *ns)
 	{
 		(*ns)
 		.add_var("result", make_namespace(regex_result_ext))
@@ -140,9 +141,9 @@ namespace regex_cs_ext {
 		.add_var("prefix", make_cni(prefix))
 		.add_var("suffix", make_cni(suffix));
 	}
-}
+}  // namespace regex_cs_ext
 
-void cs_extension_main(cs::name_space* ns)
+void cs_extension_main(cs::name_space *ns)
 {
 	regex_cs_ext::init(ns);
 }
